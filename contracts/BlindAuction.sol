c7f1111f89b24eded2b67c6077b11af35723085b
mihdjo <mihailo.djokovic2003@gmail.com>
2026-06-22 18:18:56 +0200

    Implemented full commit-reveal auction logic

--- contracts/BlindAuction.sol
+++ contracts/BlindAuction.sol
@@ -68,4 +68,88 @@ contract BlindAuction {
             })
         );
     }
+
+    error InvalidReveal();
+
+    function reveal(
+        uint[] calldata values,
+        bool[] calldata fakes,
+        bytes32[] calldata secrets
+    )
+        external
+    {
+        uint length = bids[msg.sender].length;
+        if (values.length != length) revert InvalidReveal();
+        if (fakes.length != length) revert InvalidReveal();
+        if (secrets.length != length) revert InvalidReveal();
+
+        uint refund;
+
+        for (uint i = 0; i < length; i++) {
+            Bid storage bidToCheck = bids[msg.sender][i];
+
+            (uint value, bool fake, bytes32 secret) =
+                (values[i], fakes[i], secrets[i]);
+
+            bytes32 computedHash = keccak256(
+                abi.encodePacked(value, fake, secret)
+            );
+
+            if (bidToCheck.blindedBid != computedHash) {
+                // invalid bid → ignorisati
+                continue;
+            }
+
+            refund += bidToCheck.deposit;
+
+            if (!fake && bidToCheck.deposit >= value) {
+                if (placeBid(msg.sender, value)) {
+                    refund -= value;
+                }
+            }
+
+            bidToCheck.blindedBid = bytes32(0);
+        }
+
+        payable(msg.sender).transfer(refund);
+    }
+
+    function placeBid(address bidder, uint value) internal returns (bool) {
+    if (value <= highestBid) {
+        return false;
+    }
+
+    if (highestBidder != address(0)) {
+        pendingReturns[highestBidder] += highestBid;
+    }
+
+    highestBid = value;
+    highestBidder = bidder;
+    return true;
+    }
+
+    function withdraw() external returns (bool) {
+        uint amount = pendingReturns[msg.sender];
+
+        if (amount > 0) {
+            pendingReturns[msg.sender] = 0;
+
+            if (!payable(msg.sender).send(amount)) {
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function auctionEnd() external onlyAfter(revealEnd) {
+        if (ended) return;
+
+        ended = true;
+
+        emit AuctionEnded(highestBidder, highestBid);
+
+        beneficiary.transfer(highestBid);
+    }
 }
